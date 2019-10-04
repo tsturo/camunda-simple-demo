9494f183cc9fe7ae40534011f043b66f0d11ae09
Sunil Bhambani <sunil_bhambani@persistent.co.in>
2019-10-04 23:21:52 +0530

    Initial commit

---
+++ src/main/java/com/learning/bpm/camunda/demo/CamundaSimpleDemoApplication.java
@@ -0,0 +1,26 @@
+package com.learning.bpm.camunda.demo;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaSimpleDemoApplication {
+	
+	@Autowired
+	private RuntimeService runtimeService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaSimpleDemoApplication.class, args);
+	}
+
+	@EventListener
+	private void processPostDeploy(PostDeployEvent postDeployEvent) {
+		runtimeService.startProcessInstanceByKey("Process_1h67ibv");
+	}
+}
